02db13e1823ca5fd5e255cd5c4cb1eb96b17450a
鈴木　孝伸 <>
2020-12-21 00:11:07 +0900

    feature/register

---
+++ app/Company.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
+
+class Company extends Model
+{
+    use HasApiTokens;
+
+    protected $fillable = [
+        'name',
+        'industry_class',
+        'number_of_employees',
+        'fiscal_start_date',
+        'fiscal_end_date',
+        'founded_date',
+    ];
+}
