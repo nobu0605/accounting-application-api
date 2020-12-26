26114c89ba899f85a20187f3680899425064a5ac
鈴木　孝伸 <>
2020-12-26 22:55:19 +0900

    feature/register

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Company;
+use Faker\Generator as Faker;
+
+$factory->define(Company::class, function (Faker $faker) {
+    return [
+        'name' => $faker->company,
+        'industry_class' => 'IT',
+        'number_of_employees' => $faker->randomDigit,
+        'fiscal_start_date' => $faker->dateTime,
+        'fiscal_end_date' => $faker->dateTime,
+        'founded_date' => $faker->dateTime,
+    ];
+});
